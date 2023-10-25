67bee9d407a61d2f9d4ff5137f9e6bc78b0e42a6
laboratorio <laboratorio@UPOAULA10622>
2023-10-25 07:34:40 -0500

    Creaciòn de los 5 atributos de la tabla  con nombre personalizado

---
+++ Models/AnthonyRodriguez_tabla1.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen.Models
+{
+    public class AnthonyRodriguez_tabla1
+    {
+        public int arCedula { get; set; }
+        [Required]
+        public string? arNombre { get; set; }
+        public bool arEstudiante { get; set; }
+        [Range(1.00 , 3.00)]
+        public decimal arEstatura { get; set; }
+
+        DateTime fechaActual = new DateTime(2023, 10, 25, 7, 32, 00);
+    }
+}
